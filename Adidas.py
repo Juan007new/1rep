231df87feceb8c8125c6df11fad581c5cb37d77a
Juan007new <juan.huamaccto@gmail.com>
2025-01-21 23:16:01 -0500

    Update Adidas.py

--- Adidas.py
+++ Adidas.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
 # Configurar opciones de Chrome
 chrome_options = Options()
@@ -10,6 +9,9 @@ chrome_options.add_argument('--headless')  # Modo sin GUI
 chrome_options.add_argument('--disable-gpu')  # Deshabilitar GPU (para evitar problemas en servidores)
 chrome_options.add_argument('--no-sandbox')  # Solucionar problemas en contenedores
 chrome_options.add_argument('--remote-debugging-port=9222')  # Puerto para depuración
+
+# Especificar la ubicación binaria de Chromium (asegúrate de que esté instalada en esa ruta)
+chrome_options.binary_location = '/usr/bin/chromium'  # Ajusta esta ruta según el entorno
 
 # Configuración del servicio de Selenium para ChromeDriver
 service = Service(ChromeDriverManager().install())
@@ -27,4 +29,5 @@ try:
 except Exception as e:
     print(f"Ocurrió un error: {str(e)}")
 finally:
-    driver.quit()  # Asegurarse de cerrar el navegador después de ejecutar
+    if driver:
+        driver.quit()  # Asegurarse de cerrar el navegador después de ejecutar
